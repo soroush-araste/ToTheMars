a78ae029ab7b0b152906bafe61696fe076dda80a
soroush amini <soroush.araste@gmail.com>
2022-09-11 21:51:37 +0430

    add HTTPMethods enum

---
+++ ElonMuskDream/Services/Network/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  ElonMuskDream
+//
+//  Created by soroush amini araste on 9/11/22.
+//
+
+import Foundation
+
+public enum HTTPMethod : String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+}
